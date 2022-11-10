f2220b858225bb40b749b74beb5d38deabf3bf6c
likang <972701125@qq.com>
2022-11-10 16:06:25 +0800

    pod

--- A/A/Classes/AViewController.m
+++ A/A/Classes/AViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "AViewController.h"
-#import "BViewController.h"
+#import <B_Category/CTMediator+B.h>
 #import <HandyFrame/UIView+LayoutMethods.h>
 
 @interface AViewController ()
@@ -37,7 +37,7 @@
 #pragma mark - event response
 - (void)didTappedPushBViewControllerButton:(UIButton *)button
 {
-    BViewController *viewController = [[BViewController alloc] initWithContentText:@"hello, world!"];
+    UIViewController *viewController = [[CTMediator sharedInstance] B_viewControllerWithContentText:@"hello, world!"];
     [self.navigationController pushViewController:viewController animated:YES];
 }
 
